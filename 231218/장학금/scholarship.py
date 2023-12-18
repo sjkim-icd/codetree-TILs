e887f86a94991fd7e2a5fb02a91fecd1515ce5b4
sjkim-icd <67905870+sjkim-icd@users.noreply.github.com>
2023-12-18 12:50:08 +0900

    Title: 장학금, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 231218/장학금/scholarship.py
+++ 231218/장학금/scholarship.py
@@ -4,9 +4,9 @@ b = int(temp[1])
 
 if a >= 90:
     if b >= 95:
-        print(10)
+        print(100000)
     elif b>=90:
-        print(5)
+        print(50000)
     else:
         print(0)
 else:
