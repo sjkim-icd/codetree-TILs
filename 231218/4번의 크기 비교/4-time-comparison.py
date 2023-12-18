41425deb4ef3dd8bda833c8c13c855fc7c2c341e
sjkim-icd <67905870+sjkim-icd@users.noreply.github.com>
2023-12-18 12:14:33 +0900

    Title: 4번의 크기 비교, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231218/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,12 @@
+a = int(input())
+temp = input().split()
+b = int(temp[0])
+c = int(temp[1])
+d = int(temp[2])
+e = int(temp[3])
+
+
+print(1 if a>b else 0)
+print(1 if a>c else 0)
+print(1 if a>d else 0)
+print(1 if a>e else 0)
