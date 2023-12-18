585baefec47b4df1fb1ab48fc3ee73d1390eaa7d
sjkim-icd <67905870+sjkim-icd@users.noreply.github.com>
2023-12-18 12:18:34 +0900

    Title: 점수 비교, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231218/점수 비교/score-comparison.py
@@ -0,0 +1,9 @@
+temp_1 = input().split()
+a_1 = int(temp_1[0])
+b_1 = int(temp_1[1])
+
+temp_2 = input().split()
+a_2 = int(temp_2[0])
+b_2 = int(temp_2[1])
+
+print(1 if a_1>b_1 and a_2>b_2 else 0)
