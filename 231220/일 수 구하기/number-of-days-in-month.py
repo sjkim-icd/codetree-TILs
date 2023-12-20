3c85e01adedb5f4685cf1b25f64f1b84ca3075ae
sjkim-icd <67905870+sjkim-icd@users.noreply.github.com>
2023-12-20 12:37:06 +0900

    Title: 일 수 구하기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231220/일 수 구하기/number-of-days-in-month.py
@@ -0,0 +1,14 @@
+a = int(input())
+
+if a == 2:
+    print(28)
+elif 7 >= a:
+    if a % 2 == 0:
+        print(30)
+    else: 
+        print(31)
+else:
+    if a % 2 == 0:
+        print(31)
+    else: 
+        print(30)
