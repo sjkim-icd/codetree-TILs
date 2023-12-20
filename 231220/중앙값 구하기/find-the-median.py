e9f510e04f92e259d89e338314998f4932b45ec4
sjkim-icd <67905870+sjkim-icd@users.noreply.github.com>
2023-12-20 12:53:00 +0900

    Title: 중앙값 구하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231220/중앙값 구하기/find-the-median.py
@@ -0,0 +1,14 @@
+temp = input().split()
+a = int(temp[0])
+b = int(temp[1])
+c = int(temp[2])
+
+temp_min = min(temp)
+temp_max = max(temp)
+
+if (a != temp_min) and (a != temp_max):
+    print(a)
+elif (b != temp_min) and (b != temp_max):
+    print(b)
+else:
+    print(c)
