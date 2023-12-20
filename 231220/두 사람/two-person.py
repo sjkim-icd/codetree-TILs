be0de751b98d971cf4d8701dd5482539fd7dc867
sjkim-icd <67905870+sjkim-icd@users.noreply.github.com>
2023-12-20 12:24:01 +0900

    Title: 두 사람, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231220/두 사람/two-person.py
+++ 231220/두 사람/two-person.py
@@ -7,7 +7,7 @@ b_age = int(temp1[0])
 b_gen = temp2[1]
 
 
-if (a_age >=19 or b_age  >=19) and (a_gen == 'M' or b_gen == 'M'):
+if (a_age >=19 and a_gen == 'M' ) or ( b_age  >=19 and  b_gen == 'M'):
     print(1)
 else:
     print(0)
