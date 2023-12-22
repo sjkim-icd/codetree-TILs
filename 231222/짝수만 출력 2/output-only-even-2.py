9f3f05da92ef24d47f5cbd6e262ab6113fad33f9
sjkim-icd <67905870+sjkim-icd@users.noreply.github.com>
2023-12-22 12:36:39 +0900

    Title: 짝수만 출력 2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 231222/짝수만 출력 2/output-only-even-2.py
+++ 231222/짝수만 출력 2/output-only-even-2.py
@@ -1,9 +1,9 @@
 temp = input().split()
 b = int(temp[0])  # 20
 a = int(temp[1])  # 10
-i = 20          # 20
+i = b           # 20
 
-print(i)
-while i <= 10:
+
+while i >= a:
     print(i,end=' ')
     i -= 2
