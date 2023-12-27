6805619c3814852342468941c34a36b28b582ef7
sjkim-icd <67905870+sjkim-icd@users.noreply.github.com>
2023-12-27 12:14:11 +0900

    Title: 369 게임, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 231227/369 게임/369-game.py
+++ 231227/369 게임/369-game.py
@@ -1,7 +1,13 @@
+# 변수 선언, 입력
 n = int(input())
 
-for i in range(1,n+1):
-    if (i % 3 == 0) or (i==3 or i==6 or i==9 ):
-        print(0,end=' ')
-    else: 
-        print(i,end=' ')
+# 출력
+for i in range(1, n+1):
+	if i % 3 == 0:
+		print("0", end=" ")
+	elif i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+		print("0", end=" ")
+	elif i // 10 == 3 or i // 10 == 6 or i // 10 == 9:
+		print("0", end=" ")
+	else:
+		print(i, end=" ")
