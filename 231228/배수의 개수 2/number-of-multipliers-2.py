81633ee7cb9155c243b3b47e8c080e09dbd8ae52
sjkim-icd <67905870+sjkim-icd@users.noreply.github.com>
2023-12-28 11:55:35 +0900

    Title: 배수의 개수 2, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 231228/배수의 개수 2/number-of-multipliers-2.py
+++ 231228/배수의 개수 2/number-of-multipliers-2.py
@@ -1,8 +1,9 @@
 cnt = 0
-for i in range(10):
-    if i % 2 == 1:
-        cnt +=1
-    else:
-        pass
+for _ in range(10):
+	a = int(input())
+	
+	if a % 2 == 1:
+		cnt += 1
 
+# 출력
 print(cnt)
