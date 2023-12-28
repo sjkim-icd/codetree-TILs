c598188d00bc984b9946ad6aa9d795d6eb283b4a
sjkim-icd <67905870+sjkim-icd@users.noreply.github.com>
2023-12-28 12:05:17 +0900

    Title: 특정 조건 배수의 합, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 231228/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
+++ 231228/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
@@ -1,11 +1,13 @@
 temp = input().split()
 
-a = int(temp[0])
-b = int(temp[1]) + 1
+x = int(temp[0])
+y = int(temp[1])
 
+a = min(x,y)
+b = max(x,y)
 
 sum_n = 0
-for i in range(a,b):
+for i in range(a,b+1):
     if i % 5 == 0:
         sum_n += i
     else:
