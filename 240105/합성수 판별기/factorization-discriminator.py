b49ed58965479469055d141d78279bb6c269cf66
sjkim-icd <67905870+sjkim-icd@users.noreply.github.com>
2024-01-05 11:55:25 +0900

    Title: 합성수 판별기, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240105/합성수 판별기/factorization-discriminator.py
+++ 240105/합성수 판별기/factorization-discriminator.py
@@ -1,12 +1,14 @@
 n = int(input())
 
+# 합성수란 약수가 3개 이상
 cnt_n  = 0
-for i in range(1,n):
+for i in range(1,n+1):
     if n % i == 0:
         cnt_n += 1
     else:
         pass
-if cnt_n >2:
+
+if cnt_n > 2:
     print('C')
 else:
     print('N')
